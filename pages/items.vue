f8f7e29e1c51ad592cc2aa86212d6f1a3ba64a88
Ai Shimizu <a_shimizu@tam-tam.co.jp>
2019-05-31 12:06:26 +0900

    [mod] 登録要素追加

--- pages/items.vue
+++ pages/items.vue
@@ -1,15 +1,149 @@
 <template>
-  <v-layout>
-    <v-flex text-xs-center>
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+  <v-layout row wrap justify-center align-start>
+    <v-flex xs12 sm6 md6>
+      <h2>備品登録</h2>
+      <v-layout row>
+        <v-flex>
+          <v-text-field
+            label="Solo"
+            placeholder="名字"
+            solo
+          ></v-text-field>
+        </v-flex>
+        <v-flex>
+          <v-text-field
+            label="Solo"
+            placeholder="名前"
+            solo
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+      <v-btn color="primary" dark>登録</v-btn>
+    </v-flex>
+    <v-flex xs12 sm6 md6>
+      <template>
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          :items-per-page="5"
+          class="elevation-1"
+        ></v-data-table>
+      </template>
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+  // import Logo from '~/components/Logo.vue'
+  // import VuetifyLogo from '~/components/VuetifyLogo.vue'
+  //
+  export default {
+    // components: {
+    //   Logo,
+    //   VuetifyLogo
+    // }
+    data: () => ({
+      items: ['七海春歌','渋谷友千香','一ノ瀬トキヤ', '一十木音也', '四ノ宮那月'],
+      date: new Date().toISOString().substr(0, 10),
+      menu: false,
+      modal: false,
+      menu2: false,
+      headers: [
+        {
+          text: 'Dessert (100g serving)',
+          align: 'left',
+          sortable: false,
+          value: 'name'
+        },
+        { text: 'Calories', value: 'calories' },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Iron (%)', value: 'iron' }
+      ],
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%'
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%'
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%'
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%'
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: '16%'
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: '0%'
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: '2%'
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: '45%'
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: '22%'
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: '6%'
+        }
+      ]
+    })
+  }
+
+</script>
